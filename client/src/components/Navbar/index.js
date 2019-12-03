ee9fd79424fb0b0a290d0844a25a7c2ec571830e
clmitchell630 <clmitchell630@gmail.com>
2019-12-02 20:19:41 -0600

    no working code, just saving for the night

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav class="nav">
+            <a class="nav-link" href="#">[LOGO]</a>
+            <a class="nav-link" href="#">What's this?</a>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ client/src/pages/landing.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+class Landing extends Component {
+
+    render() {
+        return (
+            <Navbar>
+                
+            </Navbar>
+
+        );
+    }
+}
+
+export default Landing;
