6567e018b43aa7aaa76550893810229e3e7fcefd
connander <connorpstark@gmail.com>
2019-12-03 20:57:51 -0600

    added register component, this handles the regsiter button as well as the app description

---
+++ client/src/components/Register/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Register() {
+    return (
+        <div id= "regButton">
+                <button variant = "primary" href = "/register">Register</button>
+        </div>
+    );
+}
+
+function Description() {
+    return (
+        <div id = "description">
+            <p className = "appDescription"> Looking for Group is designed to help bring gamers together who don't typically have friends to play with online.
+                <br/>
+                Currently, trying to find groups of new people to play with online can be a challenge.
+                <br/>
+                We aim to extend that period with the hopes of creating long lasting friendships with people you enjoy gaming with.
+                <br/>
+                We aim to put all of this into a centralized location so that we are the one stop shop for finding others to play with.
+            </p>
+        </div>
+    )
+}
+
+export default Register;
+export default Description;
--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import Register from '../components/Register'
+import Description from '../components/Register'
 
 class Landing extends Component {
 
     render() {
         return (
-            <Navbar>
-                
-            </Navbar>
+            <Container fluid>
+                <Navbar>
 
-        );
+                </Navbar>
+                <Container>
+                    <Description />
+                    <br />
+                    <br />
+                    <Register />
+                </Container>
+            </Container>
+        )
     }
 }
 
