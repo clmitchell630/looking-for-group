ec1f5cbfd1fd70625dc7b339f5a6cbd29bc32f8d
connander <connorpstark@gmail.com>
2019-12-06 16:48:14 -0600

    testing post route to db

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -3,4 +3,26 @@ const db = require("../models");
 
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/usersearch"
-)
+);
+
+const userSeed = [
+    {
+        game: "Halo Reach",
+        playLevel: "Casual",
+        troll: "No",
+        skillLevel: "1",
+        date: new Date(Date.now())
+    }
+];
+
+db.User
+.remove({})
+.then(() => db.User.collection.insertMany(UserSeed))
+.then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+})
+.catch(err => {
+    console.error(err);
+    process.exit(1);
+})
