c70b98270da4601b45f81184ed6df7b0849ca31a
clmitchell630 <clmitchell630@gmail.com>
2019-12-06 13:36:07 -0600

    fixing some broken areas

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,31 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
-import { Col, Row, Container } from '../components/Grid';
-=======
-import Navbar from '../components/Navbar';
-import Register from '../components/Register';
-import Description from '../components/Register';
->>>>>>> efff50306c2fdf12be23ec0e1dae9b3f7ee36db9
+import { Register, Description } from '../components/Register';
+
 
 class Landing extends Component {
 
     render() {
         return (
             <div>
-<<<<<<< HEAD
-                <Container>
-                
-                </Container>
-                <div className="footer">
-                    <p> Copyright © 2019 Looking for Group </p>
-                </div>
-            </div>
-        );
-=======
-                <Navbar>
-
-                </Navbar>
                 <div>
                     <Description />
                     <br />
@@ -34,8 +16,7 @@ class Landing extends Component {
                 </div>
             </div>
 
-        )
->>>>>>> efff50306c2fdf12be23ec0e1dae9b3f7ee36db9
+        );
     }
 }
 
--- client/src/pages/match.js
+++ client/src/pages/match.js
@@ -1,32 +1,21 @@
 import React, {Component} from "react";
-import ThumbsBttns from "../components/match"
+import ThumbsBttns from "../components/match";
+
 class Match extends Component {
     state = {
         lastMatch: []
     };
 
-    componentDidMount() {
-        API.getMatch()
-        .then(res => this.setState({lastMatch: res.data}))
-        .catch(err => console.log(err))
-    }
+    // componentDidMount() {
+    //     API.getMatch()
+    //     .then(res => this.setState({lastMatch: res.data}))
+    //     .catch(err => console.log(err))
+    // }
 
-<<<<<<< HEAD
-function Match() {
-    return (
-        <div>
-
-        </div>
-    );
-}
-
-export default Match;
-=======
-    handleThumbsUpButton = () => {savedMatch => {
-    }
+    handleThumbsUpButton = savedMatch => {
+    
         
     }
 }
 
-import React from "react";
->>>>>>> efff50306c2fdf12be23ec0e1dae9b3f7ee36db9
+export default Match;
