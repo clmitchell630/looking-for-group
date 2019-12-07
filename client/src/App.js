5e67ebff438ae3c4eec205f532ffb06cceac5eee
Ryan Holt <ryanholt22@yahoo.com>
2019-12-06 20:23:33 -0600

    google buttons are there, made our registration page into the google sign in component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/';
+import Signup from "./pages/signup"
+import GoogleSignIn from './components/Google Sign In';
+import Landing from './pages/landing';
 
 function App() {
   return (
-    <Navbar />
+    <GoogleSignIn />
   );
 }
 
 export default App;
+
