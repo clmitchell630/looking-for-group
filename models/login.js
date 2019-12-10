db13c6639c223263dbdcaa083bc1d859a9b93426
Ryan Holt <ryanholt22@yahoo.com>
2019-12-10 15:10:03 -0600

    replaced login button with autho login. working fine

--- models/login.js
+++ models/login.js
@@ -16,32 +16,8 @@ export class AuthStore {
     INITIAL = 'INITIAL',
     LOADING = 'LOADING',
     LOADED = 'LOADED',
-    FAILED = 'FAILED',
+    FAILED = 'FAILED'
   }
 
-class GoogleSignIn extends React.Component {
-  componentDidMount() {
-    window.gapi.signin2.render(
-      GOOGLE_BUTTON_ID,
-      {
-        width: 200,
-        height: 50,
-        onsuccess: this.onSuccess,
-      },
-    );
-  }
-
-  onSuccess(googleUser) {
-    const profile = googleUser.getBasicProfile();
-    console.log("Name: " + profile.getName());
-  }
-
-  render() {
-    return (
-      <div id={GOOGLE_BUTTON_ID}/>
-    );
-  }
-  
-}
 
 export default GoogleSignIn;
