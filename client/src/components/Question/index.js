d100d7abad9978385ea46b96a65e833d1cc51a72
Connor Stark <connorpstark@gmail.com>
2019-12-19 21:19:10 -0600

    Revert "Branch adam"

--- client/src/components/Question/index.js
+++ client/src/components/Question/index.js
@@ -67,69 +67,6 @@ class Question extends Component {
                             <option value="Runescape">Runescape</option>
                             <option value="Hearthstone">Hearthstone</option>
                             <option value="Counter-Strike">Counter-Strike</option>
-                            <option value="Anthem">Anthem</option>
-                            <option value="H1Z1">H1Z1</option>
-                            <option value="Halo Wars 2">Halo Wars 2</option>
-                            <option value="EA Sports UFC 3">EA Sports UFC 3</option>
-                            <option value="7 Days to Die">7 Days to Die</option>
-                            <option value="Ark: Survival Evolved">Ark: Survival Evolved</option>
-                            <option value="Injustice 2">Injustice 2</option>
-                            <option value="Tom Clancy's Ghost Recon Wildlands">Tom Clancy's Ghost Recon Wildlands</option>
-                            <option value="Killing Floor 2">Killing Floor 2</option>
-                            <option value="Splatoon 2">Splatoon 2</option>
-                            <option value="Overcooked!">Overcooked!</option>
-                            <option value="Mario Tennis Aces">Mario Tennis Aces</option>
-                            <option value="Tetris 99">Tetris 99</option>
-                            <option value="Payday 2">Payday 2</option>
-                            <option value="Worms W.M.D">Worms W.M.D</option>
-                            <option value="Sea of Thieves">Sea of Thieves</option>
-                            <option value="Portal 2">Portal 2</option>
-                            <option value="Dead Space 3">Dead Space 3</option>
-                            <option value="Monster Hunter: World">Monster Hunter: World</option>
-                            <option value="Assassin's Creed: Unity">Assassin's Creed: Unity</option>
-                            <option value="Syndicate">Syndicate</option>
-                            <option value="A Way Out">A Way Out</option>
-                            <option value="Absolver">Absolver</option>
-                            <option value="World War Z">World War Z</option>
-                            <option value="Need for Speed">Need for Speed</option>
-                            <option value="Sniper Elite 3">Sniper Elite 3</option>
-                            <option value="Far Cry 5">Far Cry 5</option>
-                            <option value="Dead by Daylight">Dead by Daylight</option>
-                            <option value="Paladins">Paladins</option>
-                            <option value="Team Fortress 2">Team Fortress 2</option>
-                            <option value="Dragon Ball FighterZ">Dragon Ball FighterZ</option>
-                            <option value="Unreal Tournament">Unreal Tournament</option>
-                            <option value="Quake Champions">Quake Champions</option>
-                            <option value="For Honor">For Honor</option>
-                            <option value="Tom Clancy's The Division 2">Tom Clancy's The Division 2</option>
-                            <option value="Dead Island 2">Dead Island 2</option>
-                            <option value="Dying Light">Dying Light</option>
-                            <option value="NHL 20">NHL 20</option>
-                            <option value="Heroes of the Storm">Heroes of the Storm</option>
-                            <option value="Star Wars Battlefront II">Star Wars Battlefront II</option>
-                            <option value="Gears of War 5">Gears of War 5</option>
-                            <option value="Mortal Kombat 11">Mortal Kombat 11</option>
-                            <option value="Street Fighter V">Street Fighter V</option>
-                            <option value="Tekken 7">Tekken 7</option>
-                            <option value="StarCraft II">StarCraft II</option>
-                            <option value="Diablo III">Diablo III</option>
-                            <option value="Super Smash Bros. Ultimate">Super Smash Bros. Ultimate</option>
-                            <option value="Madden NFL 20">Madden NFL 20</option>
-                            <option value="Warframe">Warframe</option>
-                            <option value="World of Tanks">World of Tanks</option>
-                            <option value="Smite">Smite</option>
-                            <option value="Red Dead Redemption 2">Red Dead Redemption 2</option>
-                            <option value="NBA 2K20">NBA 2K20</option>
-                            <option value="Rocket League">Rocket League</option>
-                            <option value="Pokemon Sword/Shield">Pokemon Sword/Shield</option>
-                            <option value="Tom Clancy's Rainbow Six: Siege">Tom Clancy's Rainbow Six: Siege</option>
-                            <option value="Grand Theft Auto V">Grand Theft Auto V</option>
-                            <option value="Titanfall 2">Titanfall 2</option>
-                            <option value="Apex Legends">Apex Legends</option>
-                            <option value="FIFA 20">FIFA 20</option>
-                            <option value="Minecraft">Minecraft</option>
-                            <option value="PUBG">PUBG</option>
-
                         </select>
                     </div>
                     <p className="pSize">Level of play</p>
@@ -160,9 +97,12 @@ class Question extends Component {
                     <div className="buttonDiv">
                         <button className="sub-btn" onClick={this.handleFormSubmit}>Save Results</button>
                     </div>
+
                 </Jumbotron>
+
             </div>
         );
     }
 }
+
 export default Question;
